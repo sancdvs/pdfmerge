cabbd0ae89bc36825a3ac53a29d6cda97dfc7f30
sancdvs <2321310864@qq.com>
2021-03-07 20:19:32 +0800

    Add files via upload

---
+++ pdfmerge.py
@@ -0,0 +1,20 @@
+import os
+from PyPDF2 import PdfFileMerger
+
+target_path = 'pdf'
+pdf_lst = [f for f in os.listdir(target_path) if f.endswith('.pdf')]
+pdf_lst = [os.path.join(target_path, filename) for filename in pdf_lst]
+
+file_merger = PdfFileMerger()
+for pdf in pdf_lst:
+    file_merger.append(pdf)     # 合并pdf文件
+
+file_merger.write('merge.pdf')
+
+
+
+
+
+
+
+
